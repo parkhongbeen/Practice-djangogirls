51db85aa119aad2ce72894dc858d46ed6ca31b7b
parkhongbeen <pack122@naver.com>
2019-12-30 18:12:44 +0900

    장고걸스 튜토리얼 복습

---
+++ djangogirls/config/urls.py
@@ -0,0 +1,34 @@
+"""config URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from blog.views import post_list, post_detail, post_add, post_delete, post_delete_confirm, post_edit, post_publish, \
+    post_unpublish
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # post-list라는 URL에 온 요청은
+    # blog.views.post_list함수가 처리한다
+    path('posts/', post_list, name='url-name-post-list'),
+    path('posts/<int:pk>/', post_detail, name='url-name-post-detail'),
+    path('posts/<int:pk>/delete/', post_delete, name='url-name-post-delete'),
+    path('posts/<int:pk>/delete/confirm/', post_delete_confirm, name='url-name-post-delete-confirm'),
+    path('posts/<int:pk>/edit/', post_edit, name='url-name-post-edit'),
+    path('posts/<int:pk>/publish/', post_publish, name='url-name-post-publish'),
+    path('posts/<int:pk>/unpublish/', post_unpublish, name='url-name-post-unpublish'),
+
+    path('posts/add/', post_add, name='url-name-post-add'),
+]
